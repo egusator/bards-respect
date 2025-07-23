64f36a7a72d12c4dfef5fff54a8124d5b3bc380b
Egor Chupin <egusator@gmail.com>
2025-07-23 22:43:16 +0700

    добавил сгенерированный на start-spring проект

---
+++ src/main/java/ru/respect/bards/BardsApplication.java
@@ -0,0 +1,13 @@
+package ru.respect.bards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/respect/bards/BardsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.respect.bards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
